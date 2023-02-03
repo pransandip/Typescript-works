71c7aed664a02827dca995a27462ee281f67bf12
pransandip <[pransandip@gmail.com]>
2023-02-03 07:34:32 +0530

    Friday, 3 Feb 2023

---
+++ 9-ninth-project-map/src/CustomMap.ts
@@ -0,0 +1,36 @@
+// Instructions to every other class
+// on how they can be an argument to 'addMarker'
+
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(
+      document.getElementById(divId) as HTMLElement,
+      {
+        zoom: 1,
+        center: {
+          lat: 0,
+          lng: 0,
+        },
+      }
+    );
+  }
+
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+  }
+}
--- 9-ninth-project-map/src/index.ts
+++ 9-ninth-project-map/src/index.ts
@@ -1,8 +1,6 @@
 import { User } from "./User";
-import { Company } from "./Company";
+// import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 
-const user = new User();
-const company = new Company();
-
-console.log(user);
-console.log(company);
+const customMap = new CustomMap("map");
+customMap.addMarker(new User());
